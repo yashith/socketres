211d96312f1e736671f0cfc8fba1645efc9857e6
yashith <yashith98@gmail.com>
2024-09-28 16:43:20 +0530

    retruns a response

--- http/http_server.c
+++ http/http_server.c
@@ -17,6 +17,7 @@
 #endif
 
 #define BUFFER_SIZE 1024
+#define PORT 1234
 void *handle_client(void *);
 void build_http_response(const char *, const char *, char *, size_t *);
 char *get_mime_type(char *);
@@ -26,21 +27,34 @@ int server_fd;
 struct sockaddr_in server_addr;
 int main(int argc, char const *argv[])
 {
+    printf("starting");
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         perror("Failed soocket");
         exit(1);
     }
 
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr=INADDR_ANY;
+    server_addr.sin_port=htons(PORT);
+
     int b = bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
-    if (b < 0)
-    {
+    if (b < 0) {
         perror("Bind fialed");
         exit(1);
     }
-    while (1)
-    {
+
+    printf("Hit here1");
+    if(listen(server_fd,10)<0){
+        perror("listen failed");
+        exit(1);
+    }
+
+    printf("Hit here3");
+    while (1) {
+
+        printf("Hit here2");
         struct sockaddr_in client_addr;
         socklen_t client_addr_len = sizeof(client_addr);
         int *client_fd = malloc(sizeof(int));
@@ -67,7 +81,7 @@ void *handle_client(void *args)
     {
         regex_t regex;
         // todo change the regex
-        regcomp(&regex, "^GET /([^])", REG_EXTENDED);
+        regcomp(&regex, "^GET /([^ ]*) HTTP/1", REG_EXTENDED);
         regmatch_t matches[2];
         if (regexec(&regex, buffer, 2, matches, 0) == 0)
         {
@@ -83,27 +97,34 @@ void *handle_client(void *args)
             char *response = (char *)malloc(BUFFER_SIZE * 2 * sizeof(char));
             size_t response_len;
             build_http_response(file_name, file_ext, response, &response_len);
-            send(client_fd, response, response_len, 0);
-            free(response);
-            free(file_name);
             */
+            char *response = (char *)malloc(BUFFER_SIZE * 2 * sizeof(char));
+            size_t response_len;
+            //remove when done
+            response = "This works";
+            //remove this when done
+            response_len = strlen(response);
+            send(client_fd, response, response_len, 0);
+            //This is requierd but why errored?
+            // free(response);
+            // free(file_name);
         }
         regfree(&regex);
     }
     close(client_fd);
     free(args);
     free(buffer);
-    retrun NULL;
+    return NULL;
 }
 
 void build_http_response(const char *file_name, const char *file_ext, char *response, size_t *response_len) {
     // const char* mime_type = get_mime_type(file_ext);
     char *header = (char *)malloc(BUFFER_SIZE * sizeof(char));
-    snprint(header, BUFFER_SIZE, "TODO send header response");
+    snprintf(header, BUFFER_SIZE, "TODO send header response");
     int file_fd = open(file_name, O_RDONLY);
 
     if (file_fd == -1) {
-        sprintf(response, BUFFER_SIZE, "TODO not found response");
+        snprintf(response, BUFFER_SIZE, "TODO not found response");
         *response_len = strlen(response);
         return;
     }
